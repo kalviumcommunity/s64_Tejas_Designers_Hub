86ae09d7437f0a3406d285d3d61ef7358c33cec1
Tejassingh29 <tejas.singh@kalvium.community>
2025-04-11 12:36:12 +0530

    backend_setup

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => res.send('DesignerHub Backend Running!'));
 
 app.listen(PORT, () => {
-  console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
 });
