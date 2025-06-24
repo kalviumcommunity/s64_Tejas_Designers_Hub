03cfb14bc8059bbe8a520f449eb3113cccef196e
Tejassingh29 <tejas.singh@kalvium.community>
2025-06-25 00:08:58 +0530

    done

--- frontend/DesignerHub/src/pages/SellerDashboard.jsx
+++ frontend/DesignerHub/src/pages/SellerDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import './SellerDashboard.css';
+import './sellerDashboard.css';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'react-toastify';
